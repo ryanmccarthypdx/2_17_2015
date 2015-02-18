2a5b5edfd44fbc36bc20ef6b39e4119e5d61e5aa
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-02-17 16:55:00 -0800

    Add full awesome functionality to tamagotchi

--- js/tamagotchi_scripts.js
+++ js/tamagotchi_scripts.js
@@ -14,21 +14,21 @@ $(function() {
         }
       },
       isHungry: function() {
-        if (this.food < 3) {
+        if (this.food <= 3) {
           return true
         } else {
           return false
         }
       },
       isSleepy: function() {
-        if (this.sleep < 3) {
+        if (this.sleep <= 3) {
           return true
         } else {
           return false
         }
       },
       isBored: function() {
-        if (this.play < 3) {
+        if (this.play <= 3) {
           return true
         } else {
           return false
@@ -41,7 +41,7 @@ $(function() {
     $("#food-level").text(tamagotchi.food);
     $("#sleep-level").text(tamagotchi.sleep);
     $("#play-level").text(tamagotchi.play);
-
+    $("#picture").html('<img src="css/img/happy.png">')
 
     $(".alive").show();
     $(".name-pic").show();
@@ -49,6 +49,22 @@ $(function() {
     event.preventDefault();
 
     setInterval(function () {
+
+      $("form#feed").submit(function(event) {
+        event.preventDefault();
+        tamagotchi.food = 10;
+      });
+
+      $("form#sleep").submit(function(event) {
+        event.preventDefault();
+        tamagotchi.sleep = 10;
+      });
+
+      $("form#play").submit(function(event) {
+        event.preventDefault();
+        tamagotchi.play = 10;
+      });
+
       tamagotchi.food = (tamagotchi.food - 1)
       tamagotchi.sleep = (tamagotchi.sleep - 1)
       tamagotchi.play = (tamagotchi.play - 1)
@@ -58,17 +74,22 @@ $(function() {
       $("#play-level").text(tamagotchi.play);
 
       if (tamagotchi.isDead() === true) {
-        $("#picture").html("<img src='css/img/dead.jpg'>")
+        $("#picture").html("<img class='picture' src='css/img/dead.jpg'>")
+        $(".alive").hide();
+        $("#input").show();
+        $(".dead").show();
       } else if (tamagotchi.isHungry() === true) {
-        $("#picture").html("<img src='css/img/hungry.png'>")
+        $("#picture").html("<img class='picture' src='css/img/hungry.png'>")
       } else if (tamagotchi.isSleepy() === true) {
-        $("#picture").html("<img src='css.img/tired.png'>")
+        $("#picture").html("<img class='picture' src='css/img/tired.png'>")
       } else if (tamagotchi.isBored() === true) {
-        $("#picture").html("<img src='css/img/bored.jpg'>")
+        $("#picture").html("<img class='picture' src='css/img/bored.jpg'>")
       } else {
-        $("#picture").html('<img src="css/img/happy.png">')
+        $("#picture").html('<img class="picture" src="css/img/happy.png">')
       }
-    }, 3000);
+    }, 2000);
+
+
 
 
 
